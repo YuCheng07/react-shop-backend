fbd2d16808d406019df8348818ff7ef4a345f428
YuCheng <aa139532058@gmail.com>
2025-03-24 22:58:56 +0800

    update

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -4,6 +4,7 @@ const db = require('../config/firebase')
 const dotenv = require('dotenv')
 dotenv.config()
 const crypto = require('crypto')
+const verifyToken = require('../middlewares/verifyToken')
 const NEWEBPAY_HASH_KEY = process.env.NEWEBPAY_HASH_KEY
 const NEWEBPAY_HASH_IV = process.env.NEWEBPAY_HASH_IV
 const NEWEBPAY_STORE_ID = process.env.NEWEBPAY_STORE_ID
@@ -11,7 +12,7 @@ const NEWEBPAY_API_URL = process.env.NEWEBPAY_API_URL
 const NEWEBPAY_NOTIFY_URL = process.env.NEWEBPAY_NOTIFY_URL
 const FRONTEND_URL = process.env.FRONTEND_URL
 
-router.post('/payment/create-order/newebpay', (req, res) => {
+router.post('/payment/create-order/newebpay', verifyToken, async (req, res) => {
 	const data = req.body
 	const timestamp = Math.round(new Date().getTime() / 1000)
 	const tradeInfo = new URLSearchParams({
@@ -36,20 +37,39 @@ router.post('/payment/create-order/newebpay', (req, res) => {
 		Version: '2.0',
 	}
 
-	res.status(200).json({
-		status: 'success',
-		message: '獲取付款連結成功',
-		data: { paymentUrl: params },
-	})
+	const userId = req.user.id
+	try {
+		const newOrder = await db.collection('orders').add({
+			id: userId,
+			orderId: parseInt(new URLSearchParams(tradeInfo).get('MerchantOrderNo')),
+			isPayed: false,
+			priceTotal: data.priceTotal,
+			cartList: data.simpleCartList,
+			orderInfo: data.orderInfo,
+			receiptInfo: data.receiptInfo,
+		})
+
+		res.status(200).json({
+			status: 'success',
+			message: '獲取付款連結成功',
+			data: { paymentUrl: params },
+		})
+	} catch (error) {
+		res.status(500).json({
+			status: 'error',
+			message: '獲取付款連結失敗',
+		})
+	}
 })
 
 router.post('/payment/return/newebpay', (req, res) => {
-	console.log(req.body)
+  console.log(res);
+  console.log(req);
+  
 	res.redirect(`${FRONTEND_URL}/checkout-success`)
 })
 
 router.post('/payment/notify/newebpay', (req, res) => {
-	console.log(req.body)
 	res.send('OK')
 })
 
